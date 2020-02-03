3c2186412bf2118d64c25b3e0dc134f7b2feea9a
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-02-03 14:17:54 -0300

    chore: prepare for Stryker support

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,34 @@
+const ignorePatterns = [
+  '/node_modules/',
+  '<rootDir>/.stryker-tmp/',
+]
+
 module.exports = {
   collectCoverage: true,
   coverageDirectory: "coverage",
-  coveragePathIgnorePatterns: [
-    "/node_modules/"
-  ],
+  coveragePathIgnorePatterns: ignorePatterns,
+  coverageReporters: ['json', 'lcovonly', 'text', 'clover'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  errorOnDeprecated: true,
   moduleDirectories: [
     "node_modules"
   ],
   moduleFileExtensions: ["js"],
   notify: true,
-  testPathIgnorePatterns: [
-    "/node_modules/"
-  ],
-  transformIgnorePatterns: [
-    "/node_modules/"
-  ],
+  roots: ['<rootDir>'],
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  testPathIgnorePatterns: ignorePatterns,
+  transform: {
+    '^.+\\.[t|j]sx?$': ['babel-jest'],
+  },
+  transformIgnorePatterns: ignorePatterns,
   watchman: true
 };
---
+++ src/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+export default function index() {
+    return 'Hello world'
+}
