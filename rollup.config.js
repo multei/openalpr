4b75b9ec00a33073148c125bd0d82fdeaabd8867
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-02-03 21:47:19 -0300

    feat(rollup.config.js) Update Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,29 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
+import json from "@rollup/plugin-json";
 
 export default [
     {
         input: 'src/index.js',
+        external: ['axios'],
         output: {
             name: 'openalpr',
             file: pkg.browser,
+            format: 'umd',
+            globals: {axios: 'axios'}
+        },
+        plugins: [
+            resolve(),
+            commonjs(),
+            json()
+        ]
+    },
+    {
+        input: 'src/toBase64.js',
+        output: {
+            name: 'toBase64',
+            file: 'dist/toBase64.js',
             format: 'umd'
         },
         plugins: [
@@ -17,7 +33,7 @@ export default [
     },
     {
         input: 'src/index.js',
-        external: ['ms'],
+        external: ['axios'],
         output: [
             { file: pkg.main, format: 'cjs' },
             { file: pkg.module, format: 'es' }
