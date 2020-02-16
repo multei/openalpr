49563185715d819543ba342ba6a34e8483f56fe8
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-02-16 20:41:44 -0300

    test(handleErrorResponse.test.js) Update tests

--- src/handleErrorResponse.js
+++ src/handleErrorResponse.js
@@ -1,4 +1,14 @@
-export default function handleErrorResponse(error) {
-    const { status, statusText, data } = error.response;
-    return { status, statusText, error_code: data.error_code, error: data.error }
+export default function handleErrorResponse(originalError) {
+
+    if(typeof originalError.response === "undefined") {
+        return {
+            status: null,
+            statusText: 'No status',
+            error_code: null,
+            error: 'Unknown error. Could not get error response data from OpenALPR API'
+        }
+    }
+
+    const { status, statusText, data: { error_code, error } } = originalError.response;
+    return { status, statusText, error_code, error }
 }
--- src/handleErrorResponse.test.js
+++ src/handleErrorResponse.test.js
@@ -1,7 +1,16 @@
 import handleErrorResponse from "./handleErrorResponse";
 
 describe('Error handler', function () {
-    it('should return error correctly', function () {
-        expect(handleErrorResponse({})).toEqual({})
+
+    const expectedKeys = ['status', 'statusText', 'error_code', 'error'];
+
+    it.each(expectedKeys)('should have %p key even when original error response is empty', key => {
+        expect(handleErrorResponse({})[key]).toBeDefined()
     })
+
+    it('should return correct error code from original error response', () => {
+        const originalError = {response: {data: {error_code: 400}}};
+        expect(handleErrorResponse(originalError).error_code).toEqual(400)
+    })
+
 })
