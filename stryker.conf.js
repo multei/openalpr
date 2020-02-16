0bf7d2df9d425167c75940d83d563520571103f7
Jimmy Andrade <jimmy.andrade@thoughtworks.com>
2020-02-16 20:42:04 -0300

    chore(stryker.conf.js) Add Stryker config keys

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,6 +1,8 @@
 module.exports = function(config) {
   config.set({
     coverageAnalysis: "off",
+    jest: {},
+    mutate: ["src/**/*.js"],
     mutator: "javascript",
     packageManager: "npm",
     reporters: ["html", "clear-text", "progress"],
